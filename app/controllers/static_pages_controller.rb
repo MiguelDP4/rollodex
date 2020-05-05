588399d281f2a138fd05f8fdc4129e42c80abd6b
migueldp4 <migueldp4@gmail.com>
2020-05-05 15:59:52 -0600

    Show posts only to friends

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     @user = current_user
     @posts = @user.friends.map { |friend| friend.posts }.flatten(1)
+    @posts += @user.posts
+    @posts = @posts.sort_by { |post| post.created_at }
+    @posts = @posts.reverse
+
+    @post = current_user.posts.build if user_signed_in?
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence:true
   validates :content, length: { in: 5..512}
+  default_scope -> { order(created_at: :desc)}
 end
