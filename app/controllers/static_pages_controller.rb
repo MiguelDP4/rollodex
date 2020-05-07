a0534255843af94b662bed6abdd4e1f5ad680b41
migueldp4 <migueldp4@gmail.com>
2020-05-06 18:15:02 -0600

    Add private and public property to posts, now public posts can be seen by every logged in user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
-    
-      # @pagy, @posts = pagy(@posts.reverse)
-    @pagy, @posts = pagy(Post.all.where(user_id: (@user.friends.pluck :id)).or(Post.all.where(user_id: (@user.id))))
+    @pagy, @posts = pagy(Post.all.where(user_id: (@user.friends.pluck :id)).or(Post.all.where(user_id: (@user.id)).or(Post.where(privacy: 'public'))))
     @post = current_user.posts.build if user_signed_in?
   end
 end
