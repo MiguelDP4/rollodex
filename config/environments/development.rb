798300b87dca03049fe189a0f86e71079b4ae04e
migueldp4 <migueldp4@gmail.com>
2020-05-11 16:22:55 -0600

    Set action mailer host for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'rollodex-development.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rollodex-development.heroku.com' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
